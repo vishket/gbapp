fa92581362c57a704137b80c20862dfd99d00a99
Vishket <vsshriwa@us.ibm.com>
2018-06-26 18:15:12 -0700

    Add requirements file and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gbapp',
+    description='A simple application to create a DB table and load data into the table a csv file.',
+    version='0.1.1',
+    author='VS',
+    packages=['gbapp',],
+
+)
